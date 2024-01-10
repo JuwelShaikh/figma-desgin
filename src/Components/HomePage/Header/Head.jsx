589e8a66adef6a18634430cf89304c73d51cf7ea
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-10 15:36:41 +0600

    header added

--- src/Components/HomePage/Header/Head.jsx
+++ src/Components/HomePage/Header/Head.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../Header/head.css'
+import arrow from '../../../Images/Arrow.png'
 
 const Head = () => {
     return (
@@ -8,7 +9,11 @@ const Head = () => {
                 <span>Stunning photography by</span>
                 <h1>Dmein Braun</h1>
             </div>
-            <div className="h-right">righ-side</div>
+            <div className="h-right">
+                <h1>Let's <button className='h-btn'><img src={arrow} alt="" /></button></h1>
+                <h1>work together</h1>
+                
+            </div>
         </div>
     );
 };
