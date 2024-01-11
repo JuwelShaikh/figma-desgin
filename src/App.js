1bbda9254f06e51591f694e54961a37e3e282761
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-11 18:09:57 +0600

    S-content styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Head from './Components/HomePage/Header/Head';
 import Navbar from './Components/HomePage/NavBar/Navbar';
+import SubContent from './Components/HomePage/SubContainer/SubContent';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Head/>
+      <SubContent/>
     </div>
   );
 }
---
+++ src/Components/HomePage/SubContainer/SubContent.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../SubContainer/Subcontent.css';
+import dot from '../../../Images/dot.png';
+
+const SubContent = () => {
+    return (
+        <div className='sub-content'>
+            <div className="s-content">
+                <img src={dot} alt="" />
+                <p>EVENT PHOTOGRAPHY</p>
+            </div>
+            <div className="s-content">
+                <img src={dot} alt="" />
+                <p>EVENT PHOTOGRAPHY</p>
+            </div>
+            <div className="s-content">
+                <img src={dot} alt="" />
+                <p>EVENT PHOTOGRAPHY</p>
+            </div>
+            <div className="s-content">
+                <img src={dot} alt="" />
+                <p>EVENT PHOTOGRAPHY</p>
+            </div>
+            <div className="s-content">
+                <img src={dot} alt="" />
+                <p>EVENT PHOTOGRAPHY</p>
+            </div>
+            <div className="s-content">
+                <img src={dot} alt="" />
+                <p>EVENT PHOTOGRAPHY</p>
+            </div>
+            <div className="s-content">
+                <img src={dot} alt="" />
+                <p>EVENT PHOTOGRAPHY</p>
+            </div>
+        </div>
+    );
+};
+
+export default SubContent;
