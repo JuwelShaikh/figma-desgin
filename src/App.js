ff6250b1b3fb6326db7fcd6a85890cf8a0ceee6b
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-13 15:28:19 +0600

    SelfContent compo added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Head from './Components/HomePage/Header/Head';
 import Navbar from './Components/HomePage/NavBar/Navbar';
 import PhotoContent from './Components/HomePage/Photo-content/PhotoContent';
+import SelfContent from './Components/HomePage/SelfContent/SelfContent';
 import SubContent from './Components/HomePage/SubContainer/SubContent';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Head/>
       <SubContent/>
       <PhotoContent/>
+      <SelfContent/>
     </div>
   );
 }
---
+++ src/Components/HomePage/SelfContent/SelfContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../SelfContent/SelfContent.css';
+
+const SelfContent = () => {
+    return (
+        <div className='main-content'>
+            
+
+        </div>
+    );
+};
+
+export default SelfContent;
