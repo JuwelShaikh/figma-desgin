c3cbaf4c60b20655f6c4f413cad7645b240a2a75
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-14 16:37:23 +0600

    Selfcontent about section added

--- src/Components/HomePage/SelfContent/SelfContent.jsx
+++ src/Components/HomePage/SelfContent/SelfContent.jsx
@@ -4,7 +4,18 @@ import '../SelfContent/SelfContent.css';
 const SelfContent = () => {
     return (
         <div className='self-container'>
-            <div className="s-container"></div>
+            <div className="s-container">
+                <div className="s-about">
+                    <div className="s-left-side">
+                        <span>about</span>
+                        <h2>i am damien</h2>
+                    </div>
+                    <div className="s-right-side">
+                        <button>Know more</button>
+                    </div>
+                    
+                </div>
+            </div>
 
         </div>
     );
