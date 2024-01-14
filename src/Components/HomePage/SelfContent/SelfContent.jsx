df46ff2262b45b2acc48b7ade6c2a1b61376ce49
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-15 00:06:27 +0600

    self abr left done

--- src/Components/HomePage/SelfContent/SelfContent.jsx
+++ src/Components/HomePage/SelfContent/SelfContent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../SelfContent/SelfContent.css';
+import selfPhoto from '../../../Images/Self photo.png'
 
 const SelfContent = () => {
     return (
@@ -13,7 +14,12 @@ const SelfContent = () => {
                     <div className="s-right-side">
                         <button>Know more</button>
                     </div>
-                    
+                </div>
+                <div className="s-abt-container">
+                    <div className="abt-left">
+                        <img src={selfPhoto} alt="" />
+                    </div>
+                    <div className="abt-right">Right</div>
                 </div>
             </div>
 
