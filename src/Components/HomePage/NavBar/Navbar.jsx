6bf78985838d6af2655a2d7acc85b5a02b11d07b
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-14 16:06:11 +0600

    Navbar position set

--- src/Components/HomePage/NavBar/Navbar.jsx
+++ src/Components/HomePage/NavBar/Navbar.jsx
@@ -4,22 +4,24 @@ import logo from '../../../Images/Logo.png'
 
 const Navbar = () => {
     return (
-        <div className='navBar'>
-            <div className="n-right">
-                <img src={logo} alt="" />
-            </div>
-            <div className="n-mid">
-                <div>
-                    <ul className="n-list">
-                        <li>Home</li>
-                        <li>About me</li>
-                        <li>Protfolio</li>
-                        <li>Services</li>
-                    </ul>
+        <div className="n-container">
+            <div className='navBar'>
+                <div className="n-right">
+                    <img src={logo} alt="" />
                 </div>
-            </div>
-            <div className="n-left">
-                <button>Contact</button>
+                <div className="n-mid">
+                    <div>
+                        <ul className="n-list">
+                            <li>Home</li>
+                            <li>About me</li>
+                            <li>Protfolio</li>
+                            <li>Services</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="n-left">
+                    <button>Contact</button>
+                </div>
             </div>
         </div>
     );
