839d1e2872fc63e434e396619399886a2d228122
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-16 17:48:16 +0600

    Highlights on work

--- src/Components/HomePage/Services/Services.jsx
+++ src/Components/HomePage/Services/Services.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../Services/Services.css'
 import leftIcon from '../../../Images/left-icon.png';
 import rightIcon from '../../../Images/Right-icon.png';
+import arrow from '../../../Images/Arrow.png';
 
 const Services = () => {
     return (
@@ -14,8 +15,25 @@ const Services = () => {
                 <div className="sRightSide">
                     <button><img src={leftIcon} alt="" /></button>
                     <button><img src={rightIcon} alt="" /></button>
-                    <button id='btn-src'>View all source</button>      
+                    <button id='btn-src'>View all source</button>
                 </div>
+            </div>
+            <div className="event-service">
+                <div className="eLeftSide">
+                    <div className="events">
+                        <div>
+                            <h1>events <button className='h-btn'><img src={arrow} alt="" /></button></h1>
+                        </div>
+                        <div className="e-content">
+                            <p>Our event photography service is dedicated to capturing the magic of your special occasions. Whether it's a wedding, corporate event, or milestone celebration, we're there to document every heartfelt moment. We blend into the background, ensuring natural and candid shots that reflect the emotions of the day.</p>
+                        </div>
+                    </div>
+                    <div className="highlights">
+                        
+                    </div>
+                </div>
+                <div className="eRightSide">right</div>
+
             </div>
         </div>
     );
