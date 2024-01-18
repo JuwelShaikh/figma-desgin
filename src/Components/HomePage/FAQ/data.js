d70bedde9d97f062036a8a35e562b3d66b62baa5
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-18 21:51:38 +0600

    FAQ added

--- src/Components/HomePage/FAQ/data.js
+++ src/Components/HomePage/FAQ/data.js
@@ -1,23 +1,23 @@
 const Questions = [
     {
-        id:1,
-        title:'hello world',
-        info:'hello world.....'
+        id: 1,
+        title: 'What type of photography do you specialize in?',
+        info: 'I specialize in [Portrait, Landscape, Event, etc.] photography, capturing moments that tell unique stories.'
     },
     {
-        id:2,
-        title:'hello world',
-        info:'hello world.....'
+        id: 2,
+        title: 'How can I book a photography session with you?',
+        info: 'No data'
     },
     {
-        id:3,
-        title:'hello world',
-        info:'hello world.....'
+        id: 3,
+        title: 'What equipment do you use for your photography?',
+        info: 'hello world.....'
     },
     {
-        id:4,
-        title:'hello world',
-        info:'hello world.....'
+        id: 4,
+        title: 'Can I request a specific location for a ',
+        info: 'hello world.....'
     }
 ];
 export default Questions;
