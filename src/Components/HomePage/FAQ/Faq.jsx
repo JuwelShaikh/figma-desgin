33e9e2c642ddf59a0c6d61169d30c49d6e781d87
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-18 15:29:41 +0600

    All file for FAQ imported

--- src/Components/HomePage/FAQ/Faq.jsx
+++ src/Components/HomePage/FAQ/Faq.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Faq.css'
+;
+import SingleQuestions from './singleQuestion';
+import data from './data';
 
 const Faq = () => {
+    const [questions, setQuestions] = useState(data);
     return (
         <div className='faq-main'>
             <div className="faq-head">
---
+++ src/Components/HomePage/FAQ/data.js
@@ -0,0 +1,23 @@
+const Questions = [
+    {
+        id:1,
+        title:'hello world',
+        info:'hello world.....'
+    },
+    {
+        id:2,
+        title:'hello world',
+        info:'hello world.....'
+    },
+    {
+        id:3,
+        title:'hello world',
+        info:'hello world.....'
+    },
+    {
+        id:4,
+        title:'hello world',
+        info:'hello world.....'
+    }
+];
+export default Questions;
