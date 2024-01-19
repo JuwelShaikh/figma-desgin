b0656a702e979a171f41d06b7e24145fa03643b0
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-19 16:41:04 +0600

    Feedback head done

--- src/Components/HomePage/Feedback/Feedback.jsx
+++ src/Components/HomePage/Feedback/Feedback.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Feedback.css';
 
 const Feedback = () => {
     return (
-        <div>
-            
+        <div className='feed-main'>
+            <div className="feed-head">
+                <div className="head-text">
+                    <span>Testimonials</span>
+                    <h4>What My Clients Say</h4>
+                </div>
+                
+            </div>
         </div>
     );
 };
