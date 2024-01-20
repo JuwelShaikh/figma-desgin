3f40d7d70a3013817d4f6a8215c642854da0982d
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-20 12:59:57 +0600

    Cards elements are added

--- src/Components/HomePage/Feedback/Feedback.jsx
+++ src/Components/HomePage/Feedback/Feedback.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import './Feedback.css';
+import leftIcon from '../../../Images/left-icon.png'
+import rightIcon from '../../../Images/Right-icon.png'
+import fb from '../../../Images/Facebook.png'
+import tw from '../../../Images/Twitter.png'
+import link from '../../../Images/Linkedin.png'
+import star from '../../../Images/star.png'
 
 const Feedback = () => {
     return (
@@ -9,7 +15,86 @@ const Feedback = () => {
                     <span>Testimonials</span>
                     <h4>What My Clients Say</h4>
                 </div>
-                
+                <div className="details">
+                    <div>
+                        <span>Total reviews</span>
+                        <p>323</p>
+                    </div>
+                    <div className="sRightSide rev">
+                        <button><img src={leftIcon} alt="" /></button>
+                        <button><img src={rightIcon} alt="" /></button>
+                        <button id='btn-src'>View all source</button>
+                    </div>
+                </div>
+            </div>
+            <div className="review-content">
+                <div className="rev-card">
+                    <div>
+                        <div>
+                            <p>Emily Johnson</p>
+                            <p>USA, California</p>
+                        </div>
+                        <div>
+                            <img src={fb} alt="" />
+                            <img src={tw} alt="" />
+                            <img src={link} alt="" />
+                        </div>
+                    </div>
+                    <div>
+                        <img src={star} alt="" />
+                        <img src={star} alt="" />
+                        <img src={star} alt="" />
+                        <img src={star} alt="" />
+                        <img src={star} alt="" />
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis aliquid odit facere esse asperiores doloremque accusantium praesentium voluptatem quasi voluptates!
+                    </div>
+                </div>
+                <div className="rev-card">
+                <div>
+                        <div>
+                            <p>Emily Johnson</p>
+                            <p>USA, California</p>
+                        </div>
+                        <div>
+                            <img src={fb} alt="" />
+                            <img src={tw} alt="" />
+                            <img src={link} alt="" />
+                        </div>
+                    </div>
+                    <div>
+                        <img src={star} alt="" />
+                        <img src={star} alt="" />
+                        <img src={star} alt="" />
+                        <img src={star} alt="" />
+                        <img src={star} alt="" />
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis aliquid odit facere esse asperiores doloremque accusantium praesentium voluptatem quasi voluptates!
+                    </div>
+                </div>
+                <div className="rev-card"><div>
+                        <div>
+                            <p>Emily Johnson</p>
+                            <p>USA, California</p>
+                        </div>
+                        <div>
+                            <img src={fb} alt="" />
+                            <img src={tw} alt="" />
+                            <img src={link} alt="" />
+                        </div>
+                    </div>
+                    <div>
+                        <img src={star} alt="" />
+                        <img src={star} alt="" />
+                        <img src={star} alt="" />
+                        <img src={star} alt="" />
+                        <img src={star} alt="" />
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis aliquid odit facere esse asperiores doloremque accusantium praesentium voluptatem quasi voluptates!
+                    </div></div>
             </div>
         </div>
     );
