54ad29bd7fc5e886544eb5e92b826a06dd54e8a6
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-21 14:52:55 +0600

    Sub footer added

--- src/Components/HomePage/SubFooter/SubFooter.jsx
+++ src/Components/HomePage/SubFooter/SubFooter.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './SubFooter.css'
+import a from '../../../Images/A.png';
+import d from '../../../Images/D.png';
+import m from '../../../Images/M.png';
+import i from '../../../Images/I.png';
+import e from '../../../Images/E.png';
+import n from '../../../Images/N.png';
+import SubContent from '../SubContainer/SubContent';
 
 const SubFooter = () => {
     return (
         <div className='foo-Main'>
-            
+            <div className="foo-wraper">
+                <img src={d} alt="" />
+                <img src={a} alt="" />
+                <img src={m} alt="" />
+                <img src={i} alt="" />
+                <img src={e} alt="" />
+                <img src={n} alt="" />
+            </div>
+            <div className="scroll">
+                <SubContent />
+            </div>
         </div>
     );
 };
