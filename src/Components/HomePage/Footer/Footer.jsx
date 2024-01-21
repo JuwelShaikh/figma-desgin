f2a220b2e0eed6c17caa16ae90093d944643e511
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-21 15:38:37 +0600

    Style

--- src/Components/HomePage/Footer/Footer.jsx
+++ src/Components/HomePage/Footer/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './footer.css';
+import design from '../../../Images/Abstract Design1.png';
+import design2 from '../../../Images/Abstract Design2.png';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='footer-main'>
+            <img src={design} alt="" />
+            <img src={design2} alt="" />
         </div>
     );
 };
